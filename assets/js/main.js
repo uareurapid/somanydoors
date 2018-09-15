f9a91412f8b67ecad83ab16538607997d1d54d2f
uareurapid <cristo.paulo@gmail.com>
2018-09-15 15:03:08 +0100

    evo: pdated site

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,27 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = "none";
+};
+
+function showModal(img) {
+	modal.style.display = "block";
+	modalImg.src = img.src;
+	captionText.innerHTML = img.alt;
+}
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
